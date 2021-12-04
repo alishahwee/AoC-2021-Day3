3554c336d6b60d9b4d62e31bb4fbf939e7ffb8b4
alishahwee <alisha.hwee@outlook.com>
2021-12-03 20:59:42 -0600

    Solve part one
    Co-authored-by: scottzyang <scottzyang@gmail.com>

--- solution.py
+++ solution.py
@@ -1,5 +1,24 @@
 def part_one(nums: list[str]) -> int:
-    pass
+    gamma_rate = ""
+    epsilon_rate = ""
+    num_length = len(nums[0])
+
+    for i in range(num_length):
+        zeros_count = 0
+        ones_count = 0
+        for num in nums:
+            if num[i] == "0":
+                zeros_count += 1
+            elif num[i] == "1":
+                ones_count += 1
+        if zeros_count > ones_count:
+            gamma_rate += "0"
+            epsilon_rate += "1"
+        else:
+            gamma_rate += "1"
+            epsilon_rate += "0"
+
+    return int(gamma_rate, 2) * int(epsilon_rate, 2)
 
 
 def part_two(nums: list[str]) -> int:
